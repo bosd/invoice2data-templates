03bd867e96e627a62ac3fabde3f4b706643f98aa
bosd <c5e2fd43-d292-4c90-9d1f-74ff3436329a@anonaddy.me>
2022-09-23 10:15:44 +0200

    disable setup

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
-setup(
-    include_package_data=True,
-    # packages=find_packages('src'),
-    # package_dir={'': 'src'},
-    package_data={
-        'invoice2data.extract': [
-            'templates/com/*.yml',
-            'templates/nl/*.yml'],
-    },
-)
+# setup(
+    # include_package_data=True,
+    # # packages=find_packages('src'),
+    # # package_dir={'': 'src'},
+    # package_data={
+        # 'invoice2data.extract': [
+            # 'templates/com/*.yml',
+            # 'templates/nl/*.yml'],
+    # },
+# )
