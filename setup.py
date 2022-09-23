85fd83ab5b86b1495585b30db36e60236648e8cf
bosd <c5e2fd43-d292-4c90-9d1f-74ff3436329a@anonaddy.me>
2022-09-23 09:46:48 +0200

    exclude src from test

--- setup.py
+++ setup.py
@@ -2,15 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     include_package_data=True,
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
+    # packages=find_packages('src'),
+    # package_dir={'': 'src'},
     package_data={
         'invoice2data.extract': [
             'templates/com/*.yml',
-            'templates/de/*.yml',
-            'templates/es/*.yml',
-            'templates/fr/*.yml',
-            'templates/nl/*.yml',
-            'templates/ch/*.yml'],
+            'templates/nl/*.yml'],
     },
 )
